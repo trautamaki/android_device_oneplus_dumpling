d2dd8785f7982931cd3416ea455d8477287619f4
Sam Mortimer <sam@mortimer.me.uk>
2021-05-25 18:31:26 -0700

    Add ih8sn
    Change-Id: I98eee9c3ec7c264cf67b8aa2da5f5bd680d69fb9

--- device.mk
+++ device.mk
@@ -51,5 +51,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.touch@1.0-service.oneplus
 
+# Safetynet
+PRODUCT_PACKAGES += ih8sn.conf.dumpling
+
+# Soong
+PRODUCT_SOONG_NAMESPACES += $(LOCAL_PATH)
+
 # Inherit from oneplus msm8998-common
 $(call inherit-product, device/oneplus/msm8998-common/common.mk)
